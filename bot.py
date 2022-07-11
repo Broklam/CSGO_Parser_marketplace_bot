d85d22c551d9a1b051b25128eb1b5bec9df483de
Ivan <ivfv@yahoo.com>
2022-07-11 02:55:29 +0200

    alpha 0.2
    - Fixed bug with NoneType.
    - Added several new parameters in message

--- bot.py
+++ bot.py
@@ -14,28 +14,42 @@ dispatcher = Dispatcher(bot)
 
 @dispatcher.message_handler(commands='start')
 async def start(message: types.Message):
-    start_buttons = ['Ножи', 'Перчатки', 'Снайперские винтовки']
+    start_buttons = ['Что это и зачем?']
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
 
     await message.answer('Выберите категорию', reply_markup=keyboard)
 
 
-@dispatcher.message_handler(Text(equals='Ножи'))
-async def get_discounted_knives(message: types.Message):
-    await message.answer('Запрос обрабатывается...')
+@dispatcher.message_handler(Text(equals='Что это и зачем?'))
+async def what(message: types.Message):
+    what_buttons = ['Предложения']
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*what_buttons)
+    await message.answer(
+        'Изучаю парсинг.\n\nБот собирает данные с биржи КС МАНИ епта, затем отбирает из них скидочные предложения.\nТам еще и инфы вдобавок достаточно.\n\nВ планах конечно сделать поиск по размеру скидки или предметам, но в данный момент поебать',reply_markup=keyboard)
+
+@dispatcher.message_handler(Text(equals='Предложения'))
+async def get_discounted(message: types.Message):
+    await message.answer('Запрос обрабатывается...\n\nЭто может занять до 5 минут.\n\nПридет кстати примерно около 100 сообщений сразу.\n\nСоветую выключить звук')
     data_collect()
+    # await message.answer(f"В данный момент обрабатывается страница {}")
     with open('result.json') as file:
         data = json.load(file)
 
     for index, item in enumerate(data):
         card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
                f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
-               f'{hbold("Цена: ")}${item.get("item_price")}'
+               f'{hbold("Цена: ")}${item.get("item_price")}\n' \
+               f'{hbold("Cтарая цена: ")}${item.get("oldPrice")}\n' \
+               f'{hbold("Деф цена: ")}${item.get("default price")}\n' \
+               f'{hbold("float: ")}{item.get("float")}\n'
+        await message.answer(card)
 
-        if index % 20 == 0:
+        if index % 30 == 0:
             time.sleep(3)
-            await message.answer(card)
+
+            # await message.answer(card)
 
 
 def main():
--- parser.py
+++ parser.py
@@ -4,7 +4,17 @@ from fake_useragent import UserAgent
 import requests
 import certifi
 
-user_agent = UserAgent(verify_ssl=False)
+user_agent = UserAgent()
+
+
+def get_3d(smth, id):
+    url1 = f'https://cs.money/skin_info?appId=730&id={id}&isBot=true&isStore=true&botInventory=true'
+    response = requests.get(
+        url=url1, headers={'user-agent': f'{user_agent.random}'})
+    data = response.json()
+    dd = data.get(smth)
+
+    return dd
 
 
 def data_collect():
@@ -15,34 +25,54 @@ def data_collect():
 
     while True:
         for item in range(offset, offset + batch_size, 60):
-            url = f'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=30&hasTradeLock=true&isStore=true&limit=60&maxPrice=10000&minPrice=100&offset={item }&sort=botFirst&tradeLockDays=1&tradeLockDays=2&tradeLockDays=3&tradeLockDays=4&tradeLockDays=5&tradeLockDays=6&tradeLockDays=7&tradeLockDays=0&withStack=true'
+            url = f'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=30&isStore=true&limit=60' \
+                  f'&maxPrice=3000.5892538833715&minPrice=150&o' \
+                  f'ffset={offset}&sort=botFirst&withStack=true '
             response = requests.get(
                 url=url, headers={'user-agent': f'{user_agent.random}'})
             offset += batch_size
             data = response.json()
             item = data.get('items')
 
-            for i in item:
-                if i.get('overprice') is not None and i.get('overprice') < -10:
-                    item_full_name = i.get('fullname')
-                    item_3d = i.get('3d')
-                    item_price = i.get('price')
-                    item_over_price = i.get('overprice')
+            try:
+                for i in item:
+                    if i.get('overprice') is not None and i.get('overprice') < -10:
+                        item_full_name = i.get('fullName')
+                        item_price = i.get('price')
+                        item_over_price = i.get('overprice')
+                        item_def_price = get_3d("defaultPrice", i.get('id'))
+                        item_disc = get_3d("discount", i.get('id'))
+                        item_float = get_3d("float", i.get('id'))
+                        item_old_price = get_3d("oldPrice",i.get('id'))
 
-                    result.append(
-                        {
-                            'full_name': item_full_name,
-                            '3d': item_3d,
-                            'overprice': item_over_price,
-                            'item_price': item_price
-                        })
-        count += 1
-        print(f'Page #{count}')
-        #print(url)
-        if len(item) < 60:
+
+                        # item_img_link = i.get('steamImg')
+                        item_3d = get_3d("3d", i.get('id'))
+                        result.append(
+                            {
+                                'full_name': item_full_name,
+                                '3d': item_3d,
+                                'overprice': item_over_price,
+                                'discount': item_disc,
+                                'default price': item_def_price,
+                                'item_price': item_price,
+                                # 'URL IMAGE': item_img_link
+                                'float': item_float,
+                                'oldPrice': item_old_price,
+                            })
+                    elif TypeError:
+                        break
+                count += 1
+                print(f'Page #{count}')
+            except ValueError:
+                pass
+            except TypeError:
+                pass
+        print(url)
+        if item is None:
             break
-        if count >10:
-            break
+        #if count == 14:
+            #break
 
     with open('result.json', 'w') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
